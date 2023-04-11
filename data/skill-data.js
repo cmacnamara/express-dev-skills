c2fabfd0054a7fed157944331c27a7f57491bdee
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-11 16:54:44 -0400

    add skills data and controller function

---
+++ data/skill-data.js
@@ -0,0 +1,9 @@
+const skills = [
+  {name: 'Git', proficient: true, _id: 257965},
+  {name: 'Express', proficient: false, _id: 175327},
+  {name: 'JavaScript', proficient: true, _id: 964357},
+]
+
+export {
+  skills
+}
