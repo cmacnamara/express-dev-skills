1edf332a0f77bd7f4890b49df7987f05bdfc5350
Chris MacNamara <cpmacnamara@gmail.com>
2023-04-14 08:57:44 -0400

    fix put route

--- controllers/skills.js
+++ controllers/skills.js
@@ -19,7 +19,8 @@ function newTodo(req,res) {
 }
 
 function create(req,res) {
-  req.body.favorite = false;
+  req.body.favorite = !!req.body.favorite;
+  console.log(req.body);
   Skill.create(req.body)
   .then(skill => {
     res.redirect('/skills')
@@ -54,10 +55,38 @@ function deleteSkill(req,res) {
   })
 }
 
+function edit(req,res) {
+  Skill.findById(req.params.skillId)
+  .then(skill => {
+    res.render('skills/edit', {
+      skill
+    })
+  })
+  .catch(error => {
+    console.log(error);
+    res.redirect('/')
+  })
+}
+
+function update(req,res) {
+  req.body.favorite = !!req.body.favorite;
+  console.log(req.body);
+  Skill.findByIdAndUpdate(req.param.skillId, req.body, {new: true})
+  .then(skill => {
+    res.redirect('/skills')
+  })
+  .catch(error => {
+    console.log(error);
+    res.redirect('/')
+  })
+}
+
 export {
   index,
   newTodo as new,
   create,
   show,
   deleteSkill as delete,
+  edit,
+  update,
 }
--- routes/skills.js
+++ routes/skills.js
@@ -5,9 +5,23 @@ const router = Router()
 
 // GET localhost:3000/skills
 router.get('/', skillsCtrl.index)
+
+// GET localhost:3000/skills/new
 router.get('/new', skillsCtrl.new)
+
+// GET localhost:3000/skills/:skillId
 router.get('/:skillId', skillsCtrl.show)
+
+// GET localhost:3000/skills
 router.post('/', skillsCtrl.create)
+
+// GET localhost:3000/skills/:skillId
 router.delete('/:skillId', skillsCtrl.delete)
+
+// GET localhost:3000/skills/:skillId/edit
+router.get('/:skillId/edit', skillsCtrl.edit)
+
+// GET localhost:3000/skills/:skillId
+router.put('/:skillId', skillsCtrl.update)
 
 export { router }
